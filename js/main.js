af75b18e16f04df5ab8cb4ee91eaef002dc79891
ernestomelchor <nurturelove88@gmail.com>
2020-08-05 23:43:00 -0500

    did not like look of ctext content, moved it to the top and added responsive font size using rem

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // DOM Elements
-const time = document.getElementById("time"),
-  greeting = document.getElementById("greeting"),
-  name = document.getElementById("name"),
-  question = document.getElementById("question"),
-  focus = document.getElementById("focus");
+const time = document.getElementById('time'),
+  greeting = document.getElementById('greeting'),
+  name = document.getElementById('name'),
+  question = document.getElementById('question'),
+  focus = document.getElementById('focus');
 
 // Options
 
@@ -18,7 +18,7 @@ function showTime() {
     sec = today.getSeconds();
 
   // Set AM or PM
-  const amPm = hour >= 12 ? "PM" : "AM";
+  const amPm = hour >= 12 ? 'PM' : 'AM';
 
   // 12hr Format
 
@@ -27,13 +27,13 @@ function showTime() {
   //Output Time
   time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
     sec
-  )}${showAmPm ? amPm : ""}`;
+  )}${showAmPm ? amPm : ''}`;
 
   setTimeout(showTime, 1000);
 }
 // Add Zeros
 function addZero(n) {
-  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+  return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
 // Set Background and Greeting
@@ -45,77 +45,77 @@ function setBackgroundGreeting() {
     // Then we know it's past Midnight
     document.body.style.backgroundImage = "url('./img/midnight.jpg')";
     greeting.textContent = "It's Past Midnight";
-    question.textContent = "What is Your Main Focus Right Now?";
-    document.body.style.color = "white";
+    question.textContent = 'What is Your Main Focus Right Now?';
+    document.body.style.color = 'white';
     // Then we know it's Dawn
   } else if (hour < 6) {
     document.body.style.backgroundImage = "url('./img/dawn.jpg')";
-    greeting.textContent = "Welcome to A New Day";
+    greeting.textContent = 'Welcome to A New Day';
   } else if (hour < 12) {
     // Then we know it's Morning
     document.body.style.backgroundImage = "url('./img/morning.jpg')";
-    greeting.textContent = "Good Morning";
+    greeting.textContent = 'Good Morning';
   } else if (hour < 18) {
     // Then we know it's the Afternoon
     document.body.style.backgroundImage = "url('./img/afternoon.jpg')";
-    greeting.textContent = "Good Afternoon";
-    document.body.style.color = "white";
+    greeting.textContent = 'Good Afternoon';
+    document.body.style.color = 'white';
   } else {
     // It's Evening
     document.body.style.backgroundImage = "url('./img/night.jpg')";
-    greeting.textContent = "Good Evening";
-    document.body.style.color = "white";
+    greeting.textContent = 'Good Evening';
+    document.body.style.color = 'white';
   }
 }
 
 // Get Name
 function getName() {
-  if (localStorage.getItem("name") === null) {
-    name.textContent = "[Enter Name]";
+  if (localStorage.getItem('name') === null) {
+    name.textContent = '[Enter Name]';
   } else {
-    name.textContent = localStorage.getItem("name");
+    name.textContent = localStorage.getItem('name');
   }
 }
 
 // Set Name
 function setName(event) {
-  if (event.type === "keypress") {
+  if (event.type === 'keypress') {
     // Make sure Enter is pressed
     if (event.which === 13 || event.keyCode === 13) {
-      localStorage.setItem("name", event.target.innerText);
+      localStorage.setItem('name', event.target.innerText);
       name.blur();
     }
   } else {
-    localStorage.setItem("name", event.target.innerText);
+    localStorage.setItem('name', event.target.innerText);
   }
 }
 
 // Get Focus
 function getFocus() {
-  if (localStorage.getItem("focus") === null) {
-    focus.textContent = "[Enter Focus]";
+  if (localStorage.getItem('focus') === null) {
+    focus.textContent = '[Enter Focus]';
   } else {
-    focus.textContent = localStorage.getItem("focus");
+    focus.textContent = localStorage.getItem('focus');
   }
 }
 
 // Set Focus
 function setFocus(event) {
-  if (event.type === "keypress") {
+  if (event.type === 'keypress') {
     // Make sure Enter is pressed
     if (event.which === 13 || event.keyCode === 13) {
-      localStorage.setItem("focus", event.target.innerText);
+      localStorage.setItem('focus', event.target.innerText);
       focus.blur();
     }
   } else {
-    localStorage.setItem("focus", event.target.innerText);
+    localStorage.setItem('focus', event.target.innerText);
   }
 }
 
-name.addEventListener("keypress", setName);
-name.addEventListener("blur", setName);
-focus.addEventListener("keypress", setFocus);
-focus.addEventListener("blur", setFocus);
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
 
 // Run
 showTime();
